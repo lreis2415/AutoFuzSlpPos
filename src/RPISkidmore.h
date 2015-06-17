652845a3580929adabd9955ef374638120f4208e
ZHULJ <crazyzlj@gmail.com>
2015-06-17 12:58:10 +0800

    add original algorithm for RPI after Skidmore,1990

---
+++ src/RPISkidmore.h
@@ -0,0 +1,19 @@
+/*!
+ * \file RPISkidmore.h
+ * \brief original algorithm for RPI after skidmore 1990
+ *
+ *
+ *
+ * \author Liangjun Zhu
+ * \version 1.0
+ * \date June 2015
+ *
+ * 
+ */
+#include "commonLib.h"
+
+typedef struct SourcePt
+{
+	int col,row;
+};
+int RPISkidmore(char *vlysrcfile,char *rdgsrcfile,int vlytag, int rdgtag, char *rpifile,char *dist2vlyfile,char *dist2rdgfile);
