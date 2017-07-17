cee5179227f4710000441445639c844b9d4394d8
crazyzlj <crazyzlj@gmail.com>
2017-07-17 22:09:23 +0800

    try mpich2 again

--- doc/ci/travis/osx_xcode/before_install.sh
+++ doc/ci/travis/osx_xcode/before_install.sh
@@ -3,9 +3,6 @@
 set -e
 brew update
 # install openmpi
-brew list openmpi &>/dev/null || brew install openmpi
+#brew list openmpi &>/dev/null || brew install openmpi
 # or install mpich2
-# however mpich2 will cause link error: ld: unknown option: -headerpad_max_install_names;
-# which is because there is a semicolon after flag -headerpad_max_install_names
-#  see details in https://github.com/OSGeo/homebrew-osgeo4mac/issues/216
-#brew install mpich2
+brew install mpich2
